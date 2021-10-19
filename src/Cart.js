76b3125072f2c4e08ad9078ddce6f4f1cb21c0a6
Isaiah Swanson <isaiahpaul.swanson@calbaptist.edu>
2021-10-18 17:32:55 -0700

    Added Cart.js and Orders.js

---
+++ src/Cart.js
@@ -0,0 +1,16 @@
+import { Container } from "react-bootstrap";
+import Navbar from "./components/Navbar";
+import React from "react";
+
+function Cart() {
+    return(
+        <div>
+            <Navbar/>
+            <div class="container">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Cart;
