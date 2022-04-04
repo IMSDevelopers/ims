6d9f391903b7fb706f49d6cc56b8d7ad4403cf5b
Isaiah Swanson <isaiahpaul.swanson@calbaptist.edu>
2022-04-04 15:43:49 -0800

    Fixed up the admin navbar and worked on the admin navigation

--- src/components/AdminNavbar.js
+++ src/components/AdminNavbar.js
@@ -2,24 +2,39 @@ import React from 'react';
 import '../styles/App.css'
 import { withRouter } from 'react-router-dom';
 
-const AdminNavbar = ({ showAddModal, history }) => {
+let pageState = "";
+
+const AdminNavbar = ({ history }) => {
 
     const redirectToOrders = () => {
         if(history) history.push('/orders');
+        pageState = "orders";
     }
 
     const redirectToHome = () => {
         if(history) history.push('/');
+        pageState = "home";
     }
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-cbu">
-            <div className="container-fluid ">
-                <button className="navbar-brand white-word" id="linkHome" onClick={() => redirectToHome(true)}>Inventory Management System</button>
-            </div>
-            <button className="btn btn-warning me-3 text-nowrap" onClick={() =>  redirectToOrders()}>Orders</button>
-        </nav>
-    );
+    if (pageState == "orders") {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-cbu">
+                <div className="container-fluid ">
+                    <button className="navbar-brand white-word" id="linkHome" onClick={() => redirectToHome(true)}>Inventory Management System</button>
+                </div>
+                <button className="btn btn-warning me-3 text-nowrap" onClick={() =>  redirectToHome()}>Home</button>
+            </nav>
+        );
+    } else {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-cbu">
+                <div className="container-fluid ">
+                    <button className="navbar-brand white-word" id="linkHome" onClick={() => redirectToHome(true)}>Inventory Management System</button>
+                </div>
+                <button className="btn btn-warning me-3 text-nowrap" onClick={() =>  redirectToOrders()}>Orders</button>
+            </nav>
+        );
+    }
 };
 
 export default withRouter(AdminNavbar);
